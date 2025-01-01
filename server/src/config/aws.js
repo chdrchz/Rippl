c7758d66c1eba570879944c4f24abe8755a70785
chdrchz <saavannaa.rose@gmail.com>
2025-01-01 13:21:27 -0600

    database set up

--- server/src/config/aws.js
+++
@@ -1,7 +0,0 @@
-const AWS = require("aws-sdk");
-
-AWS.config.update({
-  region: "your-region",
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
