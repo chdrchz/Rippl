9bc658ca528a0081c20cb2bf92c26257aeab65cb
chdrchz <saavannaa.rose@gmail.com>
2025-01-10 09:04:45 -0600

    adding updates

---
+++ client/app/updates.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function UpdatesScreen() {
+  return (
+    <View>
+      <Text>This is the Updates screen</Text>
+    </View>
+  );
+}
