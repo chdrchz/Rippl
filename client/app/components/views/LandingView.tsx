75afee6b215072912d1075b758a5f2a8ade0c932
chdrchz <saavannaa.rose@gmail.com>
2025-01-11 12:07:08 -0600

    landing view is done, just need transition animations

--- client/app/components/views/LandingView.tsx
+++ client/app/components/views/LandingView.tsx
@@ -3,18 +3,23 @@ import AppText from "../AppText";
 import ButtonOutline from "../ButtonOutline";
 import ButtonSolid from "../ButtonSolid";
 import ButtonPlainLink from "../ButtonPlainLink";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 
 export default function LandingView({ onViewChange }) {
   return (
     <View style={{ justifyContent: "center", alignItems: "center", gap: 20 }}>
       <AppText>Create an account to join our waitlist.</AppText>
-      <ButtonOutline onPress={() => onViewChange("signup")}>Sign Up</ButtonOutline>
-      <ButtonSolid onPress={() => onViewChange("signin")}>Sign In</ButtonSolid>
+      <ButtonOutline onPress={() => onViewChange("signin")}>
+        Sign In
+      </ButtonOutline>
+      <ButtonOutline onPress={() => onViewChange("signup")}>
+        Sign Up
+      </ButtonOutline>
+      <ButtonSolid onPress={() => router.push("/updates")}>Updates</ButtonSolid>
       <AppText>
         Not sure? Learn more about us{" "}
-        <ButtonPlainLink>
-          <Link href="/about">here</Link>
+        <ButtonPlainLink onPress={() => router.push("/about")}>
+          here
         </ButtonPlainLink>
       </AppText>
     </View>
